e4a7e09e2ab24cbe42bd5ccba01c60606f2cd2ca
lokesh shriwas <lokeshshriwas2802@gmail.com>
2026-05-31 00:22:09 +0530

    new apporch solution

--- 07-Binary-search-algorithm/07-find-first-and-last-idx-in-sorted-array.js
+++ 07-Binary-search-algorithm/07-find-first-and-last-idx-in-sorted-array.js
@@ -60,3 +60,45 @@ var searchRange = function (nums, target) {
   // return ans if blank then return [-1,-1]
   return ans;
 };
+
+
+/////////////////////////// Greedy Approach (same time complexity) \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\ 
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var searchRange1 = function (nums, target) {
+    let l = 0;
+    let r = nums.length - 1;
+    let ans = [-1, -1];
+    
+
+    while (l <= r) {
+        let m = l + Math.floor((r - l) / 2);
+        if (nums[m] == target) {
+            ans[0] = m;
+            r = m - 1;
+        } else if (nums[m] < target) {
+            l = m + 1;
+        } else {
+            r = m - 1;
+        }
+    }
+
+    l = 0;
+    r = nums.length - 1;
+    
+    while (l <= r) {
+        let m = l + Math.floor((r - l) / 2);
+        if (nums[m] == target) {
+            ans[1] = m;
+            l = m + 1;
+        } else if (nums[m] < target) {
+            l = m + 1;
+        } else {
+            r = m - 1;
+        }
+    }
+    return ans;
+};
